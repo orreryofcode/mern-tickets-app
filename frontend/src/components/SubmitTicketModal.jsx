2eb0bdee62ab7c89342fd0cbaf805188cd725a71
Raul Menendez <orreryofcode@gmail.com>
2022-10-31 16:34:02 -0700

    plssss

--- frontend/src/components/SubmitTicketModal.jsx
+++ frontend/src/components/SubmitTicketModal.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { createTicket } from "../features/tickets/ticketsSlice";
 
 function SubmitTicketModal({ closeModal }) {
