a15daf21679d26526709c54afa02ae71d8134d94
Raul Menendez <orreryofcode@gmail.com>
2022-11-01 13:11:43 -0700

    railway io test

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/users/";
+const API_URL = process.env.RAILWAY_URI + "/api/users/";
 
 // Register a new user
 const register = async (userData) => {
