5eb86eb498ae072d3a9fd90325d7ff16b0aa946c
Raul Menendez <orreryofcode@gmail.com>
2022-11-01 17:18:20 -0700

    cors test

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
 const { errorHandler } = require("./middleware/errorHandler");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
@@ -14,11 +15,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/tickets", require("./routes/ticketsRoute"));
 app.use("/api/users", require("./routes/usersRoute"));
 
+app.use(
+  cors({
+    origin: "https://mern-tickets-app.vercel.app/",
+    credentials: true,
+  })
+);
+
 // // Serve frontend here
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(
       path.resolve(__dirname, "../", "frontend", "build", "index.html")
     );
--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/users/";
+const API_URL = "mern-tickets-app.vercel.app/api/users/";
 
 // Register a new user
 const register = async (userData) => {
